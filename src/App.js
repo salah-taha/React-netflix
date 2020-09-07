664749a4b17064d0285399e440d72893d0117d1a
salah taha <salahtaha065@gmail.com>
2020-09-07 02:05:54 +0200

    finish the App

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from "react";
 import "./App.css";
 import Row from "./Row";
 import requests from "./requests";
+import Banner from "./Banner";
+import Nav from "./Nav";
 
 function App() {
   return (
     <div className="app">
+      <Nav />
+      <Banner />
       <Row
         title="Netflix Originals"
         isLargeRow
---
+++ src/Nav.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://raw.githubusercontent.com/CleverProgrammer/netflix_clone/49c36c007312951803b2045eb66cccdcd591d464/netflix_clone/movies/static/movies/assets/logo.svg"
+        alt="Netflix Logo"
+      />
+
+      <img
+        className="nav__avatar"
+        src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png"
+        alt="Netflix User Logo"
+      />
+    </div>
+  );
+}
+
+export default Nav;
